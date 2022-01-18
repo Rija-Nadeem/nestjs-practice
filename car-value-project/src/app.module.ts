f9b167e85c2dc35f621591e6220f969cdf171e25
Rija-Nadeem <rijanadeem42@gmail.com>
2022-01-18 15:15:25 -0800

    end to end testing

--- car-value-project/src/app.module.ts
+++ car-value-project/src/app.module.ts
@@ -1,24 +1,58 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Users } from './users/users.entity';
 import { Reports } from './reports/reports.entity';
+import { APP_PIPE } from '@nestjs/core';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [Users, Reports],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [Users, Reports],
+          synchronize: true,
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [Users, Reports],
+    //   synchronize: true,
+    // }),
     UsersModule,
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ whitelist: true }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['abc'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- car-value-project/src/main.ts
+++ car-value-project/src/main.ts
@@ -1,18 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
+// const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['abc'],
-    }),
-  );
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }))
+  // app.use(
+  //   cookieSession({
+  //     keys: ['abc'],
+  //   }),
+  // );
+  // app.useGlobalPipes(new ValidationPipe({
+  //   whitelist: true
+  // }));
   await app.listen(3000);
 }
 bootstrap();
